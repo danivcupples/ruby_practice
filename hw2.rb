c8a2eb86021fd68bb876675060b820f6a4dbe73e
Dani Cupples <danielle.volker@gmail.com>
2017-11-30 12:35:50 -0500

    first quiz method finished

--- hw2.rb
+++ hw2.rb
@@ -47,28 +47,50 @@
 
 #2. Build-a-Quiz: Build a quiz program that gets a few inputs from the user, including: title of quiz, number of questions, questions (yes or no), answers. Then let the user either build more quizzes or take a quiz. When taking a quiz, keep score!
 
-#create an empty hash
-quiz = {}
+def build_quiz
+  #create an empty hash
+  quiz = {}
 
-#user input for title of quiz
-puts "Please give me a title for your quiz."
-  #store input from the user
-  quiz[title] = gets.chomp.downcase
+  #user input for title of quiz
+  puts "Please give me a title for your quiz."
+    #store input from the user
+    quiz[:title] = gets.chomp.downcase
 
-#user input for number of questions
-puts "Please give me a number of questions."
-  #store input from user
-  quiz[num_questions] = gets.chomp.to_i
-
-#number of questions times do loop
-quiz[:num_questions].times do
-  #user input for each question
-  puts "Please provide the question."
+  #user input for number of questions
+  puts "Please give me a number of questions."
     #store input from user
-    
-    #user input for answers
+    quiz[:num_questions] = gets.chomp.to_i
+
+  #create questions array
+  question_arr = []
+
+  #create answers array
+  answer_arr = []
+
+  #number of questions times do loop
+  quiz[:num_questions].times do
+    #user input for each question
+    puts "Please provide the question."
       #store input from user
+      question_arr.push(gets.chomp.downcase)
+      #user input for answers
+      puts "Please provide the answer."
+        #store input from user
+        answer_arr.push(gets.chomp.downcase)
+
+  end
 
   #assign into key: value pair in empty hash
+  question_arr.each_with_index do |question, index|
+    q_num = "q" + (index + 1).to_s
+    quiz[q_num] = question
+  end
 
+  #assign into key: value pair in empty hash
+  answer_arr.each_with_index do |question, index|
+    a_num = "a" + (index + 1).to_s
+    quiz[a_num] = question
+  end
+
+  puts quiz
 end
